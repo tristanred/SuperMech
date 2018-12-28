394700b0a291e441385703d0a7f4e7f7c1e1acd6
Tristan Dubé <tristan.dube@gmail.com>
2018-12-28 12:57:29 -0500

    Moved project files.
    Starting to go toward a multi directory architecture. The main game files are going to be in the game/ folder and the support tools are going into the tools/ folder.

---
+++ game/src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <GameEngine.h>
+
+int main(int argc, char** argv)
+{
+    GameEngine* eng = new GameEngine();
+    eng->Initialize();
+
+    eng->Play();
+
+    printf("Super Mech !");
+
+    return 0;
+}
