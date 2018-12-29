f78584aaad17b3c08469eb6fb82495ca3ce2b5e2
Tristan Dubé <tristan.dube@gmail.com>
2018-12-29 17:31:20 -0500

    Program argument implementation
    The parser program takes an input argument to a directory with csv
    files and outputs XML files in the current debug directory.

--- tools/CSVStatsConverter/main.cpp
+++ tools/CSVStatsConverter/main.cpp
@@ -1,23 +1,62 @@
 #include "CSVBlockParser.h"
 #include "MechPartExporter.h"
+#include <libtech/sysutils.h>
+#include <libtech/arraylist.h>
+#include <libtech/fsutils.h>
+#include <libtech/pathutils.h>
 #include <stdio.h>
+
+#define TABLE_FORMAT_EXTENSION ".csv"
 
 int main(int argc, char** argv)
 {
-    CSVBlockParser parser("E:/Prog/SuperMech/doc/Memory.csv");
-    
-    auto x = parser.GetResults();
-
-
-    if (x == NULL)
+    if (argc > 1)
     {
-        printf("Error : No valid list returned.");
+        char* inputSearchDirectory = argv[1];
 
-        return 1;
+        ArrayList<char*>* filesResult = new ArrayList<char*>();
+        get_directory_files(inputSearchDirectory, false, filesResult);
+
+        for (int i = 0; i < filesResult->Count(); i++)
+        {
+            char* resultPath = filesResult->Get(i);
+            
+            char filePath[256];
+            sanitize_path_slashes(resultPath, filePath);
+
+            char* fileName = get_file_name(filePath);
+
+            char* ext = get_file_extension(filePath);
+
+            if (strcmp(ext, TABLE_FORMAT_EXTENSION) == 0)
+            {
+                CSVBlockParser parser(filePath);
+
+                auto csvResults = parser.GetResults();
+
+                if (csvResults != NULL)
+                {
+                    char* fileNameNoExt = get_file_name_noext(filePath);
+                    char outputFileName[256];
+                    sprintf(outputFileName, "%s.xml", fileNameNoExt);
+
+                    MechPartExporter exporter;
+                    exporter.ExportPartsAsXml(csvResults, outputFileName);
+
+                    //delete(fileNameNoExt); // Heap corruption, whyyyy ?
+                }
+
+            }
+
+            delete(fileName);
+            delete(ext);
+        }
+
+        return 0;
     }
 
-    MechPartExporter exporter;
-    exporter.ExportPartsAsXml(x, "output.xml");
+    printf("No directory passed in argument.");
 
-    return 0;
+    return 1;
+
 }
